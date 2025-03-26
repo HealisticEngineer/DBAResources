b3eb747eecc8eb6f4dc505cad47305dd378ac168
HealisticEngineer <johnedwardhall@gmail.com>
2025-03-26 11:48:22 +0100

    Update deadlock_output.sql
    move top 1 so that happens at first select

--- Server Query/deadlock_output.sql
+++ Server Query/deadlock_output.sql
@@ -1,12 +1,12 @@
 -- Deadlock on Azure SQL
  with XmlDeadlockReports as
  (
-	select convert(xml, event_data) as EventData
+	select TOP 1 convert(xml, event_data) as EventData
 
 	FROM sys.fn_xe_telemetry_blob_target_read_file('dl', NULL, NULL, NULL)
 	WHERE object_name = 'database_xml_deadlock_report'
  )
- select TOP 1  EventData.value('(event/@timestamp)[1]', 'datetime2(7)') as TimeStamp,
+ select EventData.value('(event/@timestamp)[1]', 'datetime2(7)') as TimeStamp,
        EventData.query('event/data/value/deadlock') as XdlFile
   from XmlDeadlockReports
  order by TimeStamp desc
@@ -15,11 +15,11 @@
  -- Deadlock on SQL Server
  with XmlDeadlockReports as
 (
-  select convert(xml, event_data) as EventData
+  select TOP 1 convert(xml, event_data) as EventData
    from sys.fn_xe_file_target_read_file(N'system_health*.xel', NULL, NULL, NULL)
   where object_name = 'xml_deadlock_report'
 ) 
-select TOP 1  EventData.value('(event/@timestamp)[1]', 'datetime2(7)') as TimeStamp,
+select EventData.value('(event/@timestamp)[1]', 'datetime2(7)') as TimeStamp,
        EventData.query('event/data/value/deadlock') as XdlFile
   from XmlDeadlockReports
  order by TimeStamp desc
